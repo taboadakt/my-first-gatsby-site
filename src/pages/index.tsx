e0653315a8f108f633063ccdd92482fd030d608f
taboadakt <taboadakt@gmail.com>
2023-01-15 17:39:37 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 // Step 1: Import React
 import * as React from "react";
-import { Link } from "gatsby";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="Waterfall image" src="../images/waterfall.jpg" />
     </Layout>
   );
 };
